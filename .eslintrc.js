8e6f89d48c17181635a2d153c44a9f2470cfe423
BinhNguyen2504 <ngvanbinh2001@gmail.com>
2022-05-30 09:50:04 +0700

    Add local fonts and animations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,69 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "react-native/react-native": true
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:import/recommended',
-    'eslint-config-prettier',
-    'airbnb',
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "eslint-config-prettier"
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: ['react', '@typescript-eslint', 'prettier', 'babel', 'import'],
+  plugins: [
+    "react",
+    "react-native",
+    "@typescript-eslint",
+    "prettier",
+    "babel",
+    "import"
+  ],
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       alias: {
-        map: [['@', './']],
-      },
-    },
+        map: [["@", "./"]]
+      }
+    }
   },
-  rules: {},
+  rules: {
+    "import/named": 2,
+    "import/namespace": 2,
+    "import/default": 2,
+    "import/export": 2,
+    "import/no-unresolved": [
+      2,
+      {
+        commonjs: true,
+        amd: true,
+        caseSensitiveStrict: true,
+        caseSensitive: true
+      }
+    ],
+    "prettier/prettier": ["error"],
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": 0,
+    "react/function-component-definition": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx", ".tsx"]
+      }
+    ],
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-raw-text": 2,
+    "react-native/no-single-element-style-arrays": 2,
+    "no-use-before-define": [
+      "error",
+      { functions: false, classes: false, variables: false }
+    ]
+  }
 };
